b4ea28b7b96f2db5f660adfc9cbbbdb9df9276a9
Charles <GUITARPLRC@YAHOO.COM>
2016-06-25 11:08:50 -0500

    add files

---
+++ script.js
@@ -0,0 +1,14 @@
+(function(){
+  
+  var title = document.getElementById("title").value,
+      search = document.getElementById("button");
+      
+  search.addEventListener("click", getInfo(title), false);
+  
+  function getInfo(title) {
+    
+    
+    
+  }
+  
+})();
